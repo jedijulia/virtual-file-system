c1f27cdcfa336327cbdd14c8615934cd55b296e1
Julia Menchavez <jcmenchavez@gmail.com>
2014-11-16 12:32:41 +0800

    edit file, rename, delete, copy, move

--- file-system.js
+++ file-system.js
@@ -15,4 +15,39 @@ function FileSystem() {
         new_file.type = 'file';
         new_file.content = content; 
     };
+
+    this.edit_file = function(name, content) {
+        var file = this.location.find(name);
+        file.content = content;
+    };
+
+    this.rename = function(name, new_name) {
+        var to_rename = this.location.find(name);
+        to_rename.value = new_name;
+    };
+
+    this.delete = function(names) {
+        for (var i=0; i < names.length; i++) {
+            var to_delete = this.location.find(names[i]);
+            this.tree.remove(to_delete);
+        }
+    };
+
+    this.copy = function(names, destination) {
+        for (var i=0; i < names.length; i++) {
+            var to_copy = this.location.find(names[i]);
+            var copy = new Node(to_copy.value);
+            copy.children = to_copy.children;
+            copy.type = to_copy.type;
+            this.tree.insert(copy, destination);
+        }
+    };
+
+    this.move = function(names, destination) {
+        for (var i=0; i < names.length; i++) {
+            var to_move = this.location.find(names[i]);
+            this.tree.remove(to_move);
+            this.tree.insert(to_move, destination);
+        }
+    };
 }
