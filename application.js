1f4ed9cc145c58bfb0f47e3aa64054ff2293ce64
Julia Menchavez <jcmenchavez@gmail.com>
2014-11-18 09:29:54 +0800

    implemented editor for new file, edit file

--- application.js
+++ application.js
@@ -33,10 +33,39 @@ $('#new-folder-button').on('click', function(e) {
 });
 
 $('#new-file-button').on('click', function(e) {
-    var output = prompt('Enter file name');
-    file_system.create_file(output, '');
-    console.log(file_system.tree.display());
-    $('.contents').append('<div class="box file"><p>' + output + '</p></div>');
+    // var output = prompt('Enter file name');
+    $('.editor').show();
+    $('.editor').attr('new-file', 'true');
+});
+
+$('.editor').on('keydown', function(e) {
+    if (e.keyCode === 83 && e.ctrlKey === true) {
+        e.preventDefault();
+        var file_title = $('input[name="file-title"]').val();
+        var file_content = $('textarea[name="file-content"]').val();
+        if (file_title !== '') {
+            var new_file = $(this).attr('new-file');
+            if (new_file === 'true') {
+                file_system.create_file(file_title, file_content);
+                console.log(file_system.tree.display());
+            } else {
+                file_system.rename(new_file, file_title);
+                file_system.edit_file(file_title, file_content);
+            }
+            $('input[name="file-title"]').val('');
+            $('textarea[name="file-content"]').val('');        
+            $('.editor').hide();
+            update_display();
+        } else {
+            alert('Please enter a title for your file.');
+        }
+    }
+});
+
+$('#close-button').on('click', function(e) {
+    $('input[name="file-title"]').val('');
+    $('textarea[name="file-content"]').val('');
+    $('.editor').hide();
 });
 
 $('.contents').on('click', '.box', function(e) {
@@ -72,6 +101,13 @@ $('.window').on('dblclick', '.box', function(e) {
         file_system.change_directory(name);
         update_display();
         console.log(file_system.location.value);
+    }
+    if ($(this).hasClass('file')) {
+        $('.editor').show();
+        var file = file_system.location.find($(this).find('p').text());
+        $('.editor').attr('new-file', file.value);
+        $('.editor input[name="file-title"]').val(file.value);
+        $('.editor textarea[name="file-content"]').val(file.content);
     }
 });
 
